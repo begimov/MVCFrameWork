e4bd4049edd3ebb3acefd0ecac4db1e9fe53efda
begimov <begimov@gmail.com>
2018-07-30 16:46:28 +0300

    AppServiceProvider's template has been created

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Providers;
+
+use League\Route\RouteCollection;
+use League\Container\ServiceProvider\AbstractServiceProvider;
+
+class AppServiceProvider extends AbstractServiceProvider
+{
+    protected $provides = [
+        //
+    ];
+
+    public function register()
+    {
+        $container = $this->getContainer();
+        
+        //
+    }
+}
