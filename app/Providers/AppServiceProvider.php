d432ec54dca23dee63aa9db25ea9b0a9fffff951
begimov <begimov@gmail.com>
2018-07-30 17:20:31 +0300

    sharing RouteCollection in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,13 +8,15 @@ use League\Container\ServiceProvider\AbstractServiceProvider;
 class AppServiceProvider extends AbstractServiceProvider
 {
     protected $provides = [
-        //
+        RouteCollection::class
     ];
 
     public function register()
     {
         $container = $this->getContainer();
         
-        //
+        $container->share(RouteCollection::class, function () use ($container) {
+            return new RouteCollection($container);
+        });
     }
 }
