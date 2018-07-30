a22503885985072c3e8fc0002a3f27ef99dd800b
begimov <begimov@gmail.com>
2018-07-30 16:05:10 +0300

    template for ConfigServiceProvider has been created

---
+++ app/Providers/ConfigServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Providers;
+use League\Container\ServiceProvider\AbstractServiceProvider;
+
+class ConfigServiceProvider extends AbstractServiceProvider
+{
+    protected $provides = [
+        'config'
+    ];
+
+    public function register()
+    {
+        $container = $this->getContainer();
+        $container->share('config', function () {
+            //
+        });
+    }
+}
