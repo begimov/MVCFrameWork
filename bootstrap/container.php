20ff81bb3ab9a7c66b5c07a7bb0715292a077f86
begimov <begimov@gmail.com>
2018-07-30 16:18:16 +0300

    autoload configuration added to composer.json, ConfigServiceProvider added to container instance

--- bootstrap/container.php
+++ bootstrap/container.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Providers\{
+    ConfigServiceProvider
+};
+
 $container = new League\Container\Container;
 
 $container->delegate(
     new League\Container\ReflectionContainer
 );
+
+$container->addServiceProvider(new ConfigServiceProvider());
