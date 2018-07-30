f552def3f267313b5acee16cef66b4f7ccbb6af7
begimov <begimov@gmail.com>
2018-07-30 16:26:52 +0300

    Config's load method implemented

--- app/Config/Config.php
+++ app/Config/Config.php
@@ -2,7 +2,19 @@
 
 namespace App\Config;
 
+use App\Config\Loaders\ILoader;
+
 class Config
 {
     protected $config = [];
+
+    public function load(array $loaders)
+    {
+        foreach ($loaders as $loader) {
+            if ($loader instanceof ILoader) {
+                $this->config = array_merge($this->config, $loader->parse());
+            }
+        }
+        return $this;
+    }
 }
