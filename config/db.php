e9fb33283606965c0caef37a36e99059c2f40ffb
begimov <begimov@gmail.com>
2018-07-31 10:15:48 +0300

    db config file created

---
+++ config/db.php
@@ -0,0 +1,16 @@
+<?php
+
+// TODO: move all private data to environmental variables
+
+return [
+    'mysql' => [
+        'driver'    => 'mysql',
+        'host'      => 'localhost',
+        'database'  => 'mvcframework',
+        'username'  => 'homestead',
+        'password'  => 'secret',
+        'charset'   => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix'    => ''
+    ]
+];
