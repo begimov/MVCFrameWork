fe17635fdc4e2667188869ba6886d7df78b50c79
begimov <begimov@gmail.com>
2018-07-31 09:52:11 +0300

    View pulled in HomeController

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controllers;
 
+use App\Views\View;
+
 class HomeController
 {
-    public function __construct()
+    protected $view;
+
+    public function __construct(View $view)
     {
-        //
+        $this->view = $view;
     }
 
     public function index($request, $response)
