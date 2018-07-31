577b0d0a543afddb9ab7d751e647353aad126385
begimov <begimov@gmail.com>
2018-07-31 10:17:49 +0300

    DatabaseServiceProvider created and added to app config

---
+++ app/Providers/DatabaseServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Providers;
+
+use League\Container\ServiceProvider\AbstractServiceProvider;
+
+class DatabaseServiceProvider extends AbstractServiceProvider
+{
+    protected $provides = [
+        //
+    ];
+
+    public function register()
+    {
+        $container = $this->getContainer();
+
+        $config = $container->get('config');
+
+        //
+    }
+}
