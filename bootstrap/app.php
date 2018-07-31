e9fc385d9005a2995e50ebe0383e6b77535c295c
begimov <begimov@gmail.com>
2018-07-31 08:17:24 +0300

    web routes required in app bootstrap file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,3 +7,5 @@ require_once __DIR__ . '/../vendor/autoload.php';
 require_once 'container.php';
 
 $route = $container->get(League\Route\RouteCollection::class);
+
+require_once __DIR__ . '/../routes/web.php';
