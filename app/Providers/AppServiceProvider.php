bf54b4b5f9a245f41749cc237ffd245044e996d3
begimov <begimov@gmail.com>
2018-07-31 09:58:20 +0300

    Response emitter shared via container

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,13 +5,16 @@ namespace App\Providers;
 use Zend\Diactoros\Response;
 use League\Route\RouteCollection;
 use Zend\Diactoros\ServerRequestFactory;
+use Zend\Diactoros\Response\SapiEmitter;
 use League\Container\ServiceProvider\AbstractServiceProvider;
 
 class AppServiceProvider extends AbstractServiceProvider
 {
     protected $provides = [
         RouteCollection::class,
-        'response'
+        'request',
+        'response',
+        'emitter'
     ];
 
     public function register()
@@ -29,5 +32,7 @@ class AppServiceProvider extends AbstractServiceProvider
                 $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
             );
         });
+
+        $container->share('emitter', SapiEmitter::class);
     }
 }
